a53f84769099c7aa11b4c3c3dcf95f25e339f773
John Pett <3016847+JohnPett@users.noreply.github.com>
2023-01-26 00:53:06 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 function init() {
+  function onNumberHover(event) {
+    const src = event.currentTarget.getAttribute('data-src');
+    const background = document.querySelector('.background');
+    const img = new Image();
+    img.onload = function() {
+      background.innerHTML = `<img src="${img.src}" />`;
+    }
+    img.src = src;
+  };
 
+  const images = [...document.querySelectorAll('.content__number')];
+  images.forEach(image => {
+    image.addEventListener('mouseover', onNumberHover);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", init);
