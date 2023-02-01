7f3d4cdc7fd25a702be59d11a07a8cff42de229e
John Pett <john.pett@toptal.com>
2023-02-02 01:28:56 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 function init() {
+  let isPlaying = false;
+  let duration = 0;
+
   const dialog = document.querySelector('dialog');
   const close = document.querySelector('.close');
   const square1 = document.querySelector('.grid__square-one');
   const circle1 = document.querySelector('.grid__circle-one');
   const square2 = document.querySelector('.grid__square-two');
   const circle2 = document.querySelector('.grid__circle-two');
+  const sound = document.querySelector('.click');
+  const playerTrack = document.querySelector('.player__track');
+  const playButton = document.querySelector('.player__play-button');
+  const playerProgress = document.querySelector('.player__progress');
 
   function debounce(func, wait, immediate) {
     var timeout;
@@ -52,14 +59,41 @@ function init() {
     square2.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1.5)}px) scale(${window.pageYOffset * 0.01})`;
     circle2.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1.75)}px) scale(${window.pageYOffset * 0.05})`;
   }
+
+  function onLinkClick(event) {
+    event.preventDefault();
+    sound.play();
+    window.location.href = event.currentTarget.getAttribute('href');
+  }
+
+  function onPlayClick() {
+    if (isPlaying) {
+      playerTrack.pause();
+      playButton.innerHTML = 'PLAY';
+    } else {
+      playerTrack.play();
+      playButton.innerHTML = 'PAUSE';
+      playerProgress.style.trasitionDuration = `${duration}s`;
+      playerProgress.style.width = '100%';
+    }
+    isPlaying = !isPlaying;
+  }
+
+  function onTrackCanPlay() {
+    duration = playerTrack.duration * 0.1;
+  }
   
   function addEventListeners() {
     const videos = [...document.querySelectorAll('video')];
     const images = [...document.querySelectorAll('img')];
+    const hrefs = [...document.querySelectorAll('a')];
     images.forEach(image => image.addEventListener('click', onImageClick));
     videos.forEach(video => video.addEventListener('click', onVideoClick));
+    hrefs.forEach(href => href.addEventListener('click', onLinkClick));
     close.addEventListener('click', onCloseClick);
-    window.addEventListener('scroll', onScroll);
+    playButton.addEventListener('click', onPlayClick);
+    playerTrack.addEventListener('canplay', onTrackCanPlay);
+    // window.addEventListener('scroll', onScroll);
   }
 
   addEventListeners();
