b94e187baca506e7b8b77a6c7df7dd05977717c7
19ini91 <i.melean@yahoo.de>
2023-02-03 23:51:06 +0100

    added all assets + added fonts 2 CSS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
 function init() {
   const dialog = document.querySelector('dialog');
   const close = document.querySelector('.close');
+  const square1 = document.querySelector('.grid__square-one');
+  const circle1 = document.querySelector('.grid__circle-one');
+  const square2 = document.querySelector('.grid__square-two');
+  const circle2 = document.querySelector('.grid__circle-two');
 
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var contect = this, args = arguments;
+      var later = function () {
+        timeout = null;
+        if (callNow) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  
   function onCloseClick() {
     dialog.innerHTML = '';
     dialog.style.display = 'none';
     close.style.display = 'none';
   }
   
-  function onImageClick(event) { // On each image clicked give me the corresponding event
-    const src = event.currentTarget.getAttribute('src'); // Grabbing the source of the image clicked
-    const img = new Image(); // Create HTML <img> tag with JS
-  
-    img.onload = function() { // Once my image has loaded...
-      dialog.innerHTML = `<img src="${img.src}" />`;  // Go find the <dialog> previously defined above
-      dialog.style.display = 'flex'; // Show the hidden dialog box
+  function onImageClick(event) {
+    const src = event.currentTarget.getAttribute('src');
+    const img = new Image();
+    img.onload = function() {
+      dialog.innerHTML = `<img src="${img.src}" />`;
+      dialog.style.display = 'flex';
     }
-  
-    img.src = src; // Define the src attribute of the <img src="_image_source_" />
-
+    img.src = src;
     close.style.display = 'block';
   }
 
@@ -28,16 +45,25 @@ function init() {
     dialog.style.display = 'flex';
     close.style.display = 'block';
   }
+
+  function onScroll() {
+    const windowHeight = window.innerHeight;
+    square1.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1.5)}px) scale(${window.pageYOffset * 0.05})`;
+    circle1.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1)}px) scale(${window.pageYOffset * 0.01})`;
+    square2.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1.5)}px) scale(${window.pageYOffset * 0.01})`;
+    circle2.style.transform = `translateY(${windowHeight - (window.pageYOffset * 1.5)}px) scale(${window.pageYOffset * 0.01})`;
+  }
   
   function addEventListeners() {
     const videos = [...document.querySelectorAll('video')];
-    const images = [...document.querySelectorAll('img')]; // Query the whole document for imgs, then put them in an array
-    images.forEach(image => image.addEventListener('click', onImageClick)); // For each image, create a unique 'click' event
+    const images = [...document.querySelectorAll('img')];
+    images.forEach(image => image.addEventListener('click', onImageClick));
     videos.forEach(video => video.addEventListener('click', onVideoClick));
     close.addEventListener('click', onCloseClick);
+    window.addEventListener('scroll', onScroll);
   }
 
-  addEventListeners(); // I'm loaded, now add events to HTML elements
+  addEventListeners();
 }
 
-document.addEventListener("DOMContentLoaded", init); // Once all HTML has loaded, fire an event called init()
+document.addEventListener("DOMContentLoaded", init);
